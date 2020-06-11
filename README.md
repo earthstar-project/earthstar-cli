2a50717a1f9f2cdb9459a41ebabd17840914e5dd
Cinnamon <cinnamon_bun_github@fastmail.com>
2020-06-11 16:55:53 -0700

    revise to work with earthstar 1.0.0.  syncing is turned off for the moment.

--- README.md
+++ README.md
@@ -10,12 +10,12 @@ earthstar create-database demo.sqlite demo
 
 Create an author keypair
 ```
-earthstar generate-author > author-keypair.json
+earthstar generate-author suzy > author-keypair.json
 ```
 
-Set a key
+Set a path
 ```
-earthstar set demo.sqlite author-keypair.json key1 value1
+earthstar set demo.sqlite author-keypair.json path1 value1
 ```
 
 See what's in a workspace
@@ -34,43 +34,39 @@ earthstar sync demo.sqlite demo2.sqlite
 
 Sync with an earthstar-pub.
 ```
-earthstar sync demo.sqlite https://example.com/earthstar/
+earthstar sync demo.sqlite https://example.com
 ```
 
 ## Usage
 
 Arguments:
-* `<db>`: filename to an sqlite file
-* `<workspace>`: any string
+* `<dbFilename>`: filename to an sqlite file
+* `<workspace>`: a workspace address like `//gardening.ac9eEIhf9332He0afwf`
 * `<authorFile>`: a JSON file in the format printed by `generate-author`
-* `<key>`: any string
+* `<path>`: an Earthstar path, starting with a slash
 * `<value>`: any string
-* `<url>`: HTTP address of an earthstar pub.  Must end in `/earthstar/`.
+* `<url>`: HTTP address of an earthstar pub.
 
 ```
 Usage: earthstar [options] [command]
 
 Options:
-  --unsigned                           Allow/create messages of type "unsigned.1" which
-                                       do not have signatures.  This is insecure.  Only
-                                       use it for testing.
-  -h, --help                           display help for command
+  -h, --help                                   display help for command
 
 Commands:
-  generate-author                      Generate and print a new author keypair
-  create-database <db> <workspace>     Create a new sqlite database to hold a given
-                                       workspace
-  info <db>                            Report basic info about the database
-  pairs <db>                           Show keys and values
-  keys <db>                            List the keys
-  items <db>                           List the items in a database including history
-                                       items
-  values <db>                          List the values in a database (sorted by their
-                                       key)
-  authors <db>                         List the authors in a database
-  set <db> <authorFile> <key> <value>  Set a key to a value.  authorFile should be a
-                                       JSON file.
-  sync <dbOrUrl1> <dbOrUrl2>           Sync between two local files and/or remote
-                                       servers.  Urls should end in "/earthstar/"
-  help [command]                       display help for command
+  generate-author <shortname>                  Generate and print a new author keypair with
+                                               the given 4-letter shortname
+  create-workspace <dbFilename> <workspace>    Create a new sqlite database file to hold a
+                                               given workspace
+  info <dbFilename>                            Report basic info about the workspace
+  pairs <dbFilename>                           Show paths and values
+  paths <dbFilename>                           List the paths
+  documents <dbFilename>                       List the documents in a workspace including
+                                               history documents
+  values <dbFilename>                          List the values in a workspace (sorted by
+                                               their path)
+  authors <dbFilename>                         List the authors in a workspace
+  set <dbFilename> <authorFile> <key> <value>  Set a value at a path.  authorFile should be
+                                               a JSON file.
+  help [command]                               display help for command
 ```
--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,23 @@
 import { readFileSync } from 'fs';
 import commander = require('commander');
 import fetch from 'node-fetch';
+
 import {
-    Crypto,
     IValidator,
-    StoreSqlite,
-    ValidatorEs1,
-    ValidatorUnsigned1,
+    StorageSqlite,
+    ValidatorEs2,
+    generateAuthorKeypair,
 } from 'earthstar';
 
 //================================================================================
 // HELPERS
 
+/*
 let syncLocalAndHttp = async (db : string, url : string) => {
-    let es = new StoreSqlite({
+    let es = new StorageSqlite({
         mode: 'open',
         workspace: null,
-        validators: [ValidatorEs1],
+        validators: [ValidatorEs2],
         filename: db,
     });
     console.log('existing database workspace:', es.workspace);
@@ -45,14 +46,14 @@ let syncLocalAndHttp = async (db : string, url : string) => {
     if (resp.status === 404) {
         console.log('    server 404: server does not know about this workspace yet');
     } else {
-        let items = await resp.json();
+        let docs = await resp.json();
         let pullStats = {
             numIngested: 0,
             numIgnored: 0,
-            numTotal: items.length,
+            numTotal: docs.length,
         };
-        for (let item of items) {
-            if (es.ingestItem(item)) { pullStats.numIngested += 1; }
+        for (let doc of docs) {
+            if (es.ingestDocument(doc)) { pullStats.numIngested += 1; }
             else { pullStats.numIgnored += 1; }
         }
         console.log(JSON.stringify(pullStats, null, 2));
@@ -81,148 +82,151 @@ let syncLocalAndHttp = async (db : string, url : string) => {
         console.log(JSON.stringify(pushStats, null, 2));
     }
 };
+*/
 
 //================================================================================
 
-let getValidators = (insecure : boolean | undefined) => {
-    let vals : IValidator[] = [ValidatorEs1];
-    if (insecure === true) {
-        vals.push(ValidatorUnsigned1);
-        console.log('WARNING: Allowing unsigned items.  This is insecure.');
-    }
-    return vals;
-}
+let VALIDATORS = [ValidatorEs2];
 
 let app = new commander.Command();
 
 app
     .name('earthstar')
-    .option('--unsigned', 'Allow/create messages of type "unsigned.1" which do not have signatures.  This is insecure.  Only use it for testing.');
 app
-    .command('generate-author')
-    .description('Generate and print a new author keypair')
-    .action(() => {
-        console.log(JSON.stringify(Crypto.generateKeypair(), null, 2));
+    .command('generate-author <shortname>')
+    .description('Generate and print a new author keypair with the given 4-letter shortname')
+    .action((shortname) => {
+        // TODO: this check should happen in earthstar core, in generateAuthorKeypair
+        if (shortname.length !== 4) {
+            console.warn('ERROR: shortname must be exactly 4 lowercase letters.');
+            process.exit(1);
+        }
+        console.log(JSON.stringify(generateAuthorKeypair(shortname), null, 2));
     });
 app
-    .command('create-database <db> <workspace>')
-    .description('Create a new sqlite database to hold a given workspace')
-    .action((db, workspace) => {
-        let es = new StoreSqlite({
+    .command('create-workspace <dbFilename> <workspace>')
+    .description('Create a new sqlite database file to hold a given workspace')
+    .action((dbFilename, workspace) => {
+        // TODO: verify workspace is parsable
+        let storage = new StorageSqlite({
             mode: 'create',
             workspace: workspace,
-            validators: getValidators(app.unsigned),
-            filename: db,
+            validators: VALIDATORS,
+            filename: dbFilename,
         });
     });
 app
-    .command('info <db>')
-    .description('Report basic info about the database')
-    .action((db : string) => {
-        let es = new StoreSqlite({
+    .command('info <dbFilename>')
+    .description('Report basic info about the workspace')
+    .action((dbFilename : string) => {
+        let storage = new StorageSqlite({
             mode: 'open',
             workspace: null,
-            validators: getValidators(app.unsigned),
-            filename: db,
+            validators: VALIDATORS,
+            filename: dbFilename,
         });
         console.log(JSON.stringify({
-            workspace: es.workspace,
-            num_authors: es.authors().length,
-            num_keys: es.keys().length,
-            num_items_including_history: es.items({ includeHistory: true }).length
+            workspace: storage.workspace,
+            num_authors: storage.authors().length,
+            num_paths: storage.paths().length,
+            num_documents_including_history: storage.documents({ includeHistory: true }).length
         }, null, 2));
     });
 app
-    .command('pairs <db>')
-    .description('Show keys and values')
-    .action((db : string) => {
-        let es = new StoreSqlite({
+    .command('pairs <dbFilename>')
+    .description('Show paths and values')
+    .action((dbFilename : string) => {
+        let storage = new StorageSqlite({
             mode: 'open',
             workspace: null,
-            validators: getValidators(app.unsigned),
-            filename: db,
+            validators: VALIDATORS,
+            filename: dbFilename,
         });
-        for (let item of es.items()) {
-            console.log(item.key);
-            console.log('    ' + item.value);
+        for (let doc of storage.documents()) {
+            console.log(doc.path);
+            console.log('    ' + doc.value);
         }
     });
 app
-    .command('keys <db>')
-    .description('List the keys')
-    .action((db : string) => {
-        let es = new StoreSqlite({
+    .command('paths <dbFilename>')
+    .description('List the paths')
+    .action((dbFilename : string) => {
+        let storage = new StorageSqlite({
             mode: 'open',
             workspace: null,
-            validators: getValidators(app.unsigned),
-            filename: db,
+            validators: VALIDATORS,
+            filename: dbFilename,
         });
-        for (let key of es.keys()) {
-            console.log(key);
+        for (let path of storage.paths()) {
+            console.log(path);
         }
     });
 app
-    .command('items <db>')
-    .description('List the items in a database including history items')
-    .action((db : string) => {
-        let es = new StoreSqlite({
+    .command('documents <dbFilename>')
+    .description('List the documents in a workspace including history documents')
+    .action((dbFilename : string) => {
+        let storage = new StorageSqlite({
             mode: 'open',
             workspace: null,
-            validators: getValidators(app.unsigned),
-            filename: db,
+            validators: VALIDATORS,
+            filename: dbFilename,
         });
-        for (let item of es.items({ includeHistory: true })) {
+        for (let item of storage.documents({ includeHistory: true })) {
             console.log(JSON.stringify(item, null, 2));
         }
     });
 app
-    .command('values <db>')
-    .description('List the values in a database (sorted by their key)')
-    .action((db : string) => {
-        let es = new StoreSqlite({
+    .command('values <dbFilename>')
+    .description('List the values in a workspace (sorted by their path)')
+    .action((dbFilename : string) => {
+        let storage = new StorageSqlite({
             mode: 'open',
             workspace: null,
-            validators: getValidators(app.unsigned),
-            filename: db,
+            validators: VALIDATORS,
+            filename: dbFilename,
         });
-        for (let value of es.values()) {
+        for (let value of storage.values()) {
             console.log(value);
         }
     });
 app
-    .command('authors <db>')
-    .description('List the authors in a database')
-    .action((db : string) => {
-        let es = new StoreSqlite({
+    .command('authors <dbFilename>')
+    .description('List the authors in a workspace')
+    .action((dbFilename : string) => {
+        let storage = new StorageSqlite({
             mode: 'open',
             workspace: null,
-            validators: getValidators(app.unsigned),
-            filename: db,
+            validators: VALIDATORS,
+            filename: dbFilename,
         });
-        for (let author of es.authors()) {
+        for (let author of storage.authors()) {
             console.log(author);
         }
     });
 app
-    .command('set <db> <authorFile> <key> <value>')
-    .description('Set a key to a value.  authorFile should be a JSON file.')
-    .action((db, authorFile, key, value) => {
-        let es = new StoreSqlite({
+    .command('set <dbFilename> <authorFile> <key> <value>')
+    .description('Set a value at a path.  authorFile should be a JSON file.')
+    .action((dbFilename, authorFile, path, value) => {
+        let storage = new StorageSqlite({
             mode: 'open',
             workspace: null,
-            validators: getValidators(app.unsigned),
-            filename: db,
+            validators: VALIDATORS,
+            filename: dbFilename,
         });
         let keypair = JSON.parse(readFileSync(authorFile, 'utf8'));
-        let success = es.set(keypair, {
-            format: app.unsigned === true ? 'unsigned.1' : 'es.1',
-            key,
-            value,
-        });
+        let success = storage.set(
+            keypair,
+            {
+                format: 'es.2',
+                path: path,
+                value,
+            });
         if (!success) {
             console.error('ERROR: set failed');
         }
     });
+
+/*
 app
     .command('sync <dbOrUrl1> <dbOrUrl2>')
     .description('Sync between two local files and/or remote servers.  Urls should end in "/earthstar/"')
@@ -238,16 +242,16 @@ app
             await syncLocalAndHttp(db, url);
         } else if (!isUrl(dbOrUrl1) && !isUrl(dbOrUrl2)) {
             // two local files
-            let es1 = new StoreSqlite({
+            let es1 = new StorageSqlite({
                 mode: 'open',
                 workspace: null,
-                validators: getValidators(app.unsigned),
+                validators: VALIDATORS,
                 filename: dbOrUrl1,
             });
-            let es2 = new StoreSqlite({
+            let es2 = new StorageSqlite({
                 mode: 'open',
                 workspace: null,
-                validators: getValidators(app.unsigned),
+                validators: VALIDATORS,
                 filename: dbOrUrl2,
             });
             if (es1.workspace !== es2.workspace) {
@@ -263,7 +267,7 @@ app
             console.error('NOT IMPLEMENTED YET: sync between two urls');
         }
     });
-
+*/
 app.parse(process.argv);
 
 
