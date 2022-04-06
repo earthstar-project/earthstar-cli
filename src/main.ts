e3849ca4d77be7c86526b862ecef222225a1ad67
Sam Gwilym <gwilym@me.com>
2022-04-06 12:48:27 +0000

    Update version number

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { registerShareCommands } from "./share.ts";
 
 const mainCommand = new Cliffy.Command()
   .name("earthstar")
-  .version("2.0.0").description(
+  .version("7.0.0").description(
     "Sync, view, and write data to Earthstar shares.",
   ).command("completions", new Cliffy.CompletionsCommand());
 
